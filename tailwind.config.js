770890b43a5c3f8d1aa7f893aa97d11cd9ec7d3d
Misbah Ansori <misbah.ansori24@gmail.com>
2022-05-31 13:22:48 +0700

    Install Gilroy font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Gilroy", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
