52238f7bd40cd3e427b451b65102b6af36ba0918
Misbah Ansori <misbah.ansori24@gmail.com>
2022-05-31 14:50:12 +0700

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,71 @@ import DeviceMobileIcon from "./icons/device-mobile.svg?component";
 import DeviceMobilerotatedIcon from "./icons/device-mobile-rotated.svg?component";
 import PackageIcon from "./icons/package.svg?component";
 import UsersIcon from "./icons/users.svg?component";
+import SquarePlusIcon from "./icons/square-plus.svg?component";
+import LayoutIcon from "./icons/layout.svg?component";
+import Palette from "./icons/palette.svg?component";
+import FileIcon from "./icons/file.svg?component";
+import ImageIcon from "./icons/image.svg?component";
+import AlertCircleIcon from "./icons/alert-circle.svg?component";
+import SettingsIcon from "./icons/settings.svg?component";
 
 function App() {
   return (
     <div className="flex bg-gray-100 font-sans text-gray-900">
-      <aside className="flex min-h-screen w-18 flex-col items-center border border-gray-200 bg-white">
-        <div className="flex h-[79px] w-full items-center justify-center border-b border-gray-200">
+      <aside className="flex min-h-screen w-18 flex-col items-center border-r border-gray-200 bg-white">
+        <div className="flex h-20 w-full items-center justify-center border-b border-gray-200">
           <LogoIcon />
+        </div>
+        <nav className="flex flex-1 flex-col gap-y-4 pt-10">
+          <a
+            href="#"
+            className="rounded-2xl bg-gray-100 p-2 text-blue-500 hover:bg-gray-50"
+          >
+            <SquarePlusIcon className="h-6 w-6 stroke-current" />
+          </a>
+          <a
+            href="#"
+            className="rounded-2xl p-2 text-gray-400 hover:bg-gray-100"
+          >
+            <LayoutIcon className="h-6 w-6 stroke-current" />
+          </a>
+          <a
+            href="#"
+            className="rounded-2xl p-2 text-gray-400 hover:bg-gray-100"
+          >
+            <Palette className="h-6 w-6 stroke-current" />
+          </a>
+          <a
+            href="#"
+            className="rounded-2xl p-2 text-gray-400 hover:bg-gray-100"
+          >
+            <FileIcon className="h-6 w-6 stroke-current" />
+          </a>
+          <a
+            href="#"
+            className="rounded-2xl p-2 text-gray-400 hover:bg-gray-100"
+          >
+            <ImageIcon className="h-6 w-6 fill-current" />
+          </a>
+        </nav>
+        <div className="flex flex-col items-center gap-y-4 py-10">
+          <button className="rounded-2xl p-2 text-gray-400 hover:bg-gray-100">
+            <AlertCircleIcon className="h-6 w-6 stroke-current" />
+          </button>
+          <button className="rounded-2xl p-2 text-gray-400 hover:bg-gray-100">
+            <SettingsIcon className="h-6 w-6 stroke-current" />
+          </button>
+          <button className="mt-2 rounded-full bg-gray-100">
+            <img
+              className="h-10 w-10 object-cover"
+              src="/img/avatar.png"
+              alt=""
+            />
+          </button>
         </div>
       </aside>
       <div className="flex min-h-screen flex-1 flex-col">
-        <header className="flex h-20 items-center justify-between gap-x-6 border-y border-gray-200 bg-white px-6">
+        <header className="flex h-20 items-center justify-center gap-x-6 border-b border-gray-200 bg-white px-8">
           <button className="flex items-center justify-center rounded-2xl bg-gray-100 p-3">
             <ChevronLeftIcon className="h-6 w-6 stroke-current text-gray-400" />
           </button>
@@ -41,18 +95,18 @@ function App() {
           {/* Separator */}
           <div className="h-full w-px bg-gray-200" />
 
-          <div className="flex items-center gap-x-1">
-            <button className="rounded-2xl bg-gray-100 p-2">
-              <DeviceDesktopIcon className="h-6 w-6 stroke-current text-gray-400" />
+          <div className="flex items-center gap-x-2">
+            <button className="rounded-2xl bg-gray-100 p-3 text-blue-500">
+              <DeviceDesktopIcon className="h-6 w-6 stroke-current" />
             </button>
-            <button className="rounded-2xl  p-2">
-              <DeviceTabletIcon className="h-6 w-6 stroke-current text-gray-400" />
+            <button className="rounded-2xl p-3 text-gray-400 hover:bg-gray-100">
+              <DeviceTabletIcon className="h-6 w-6 stroke-current" />
             </button>
-            <button className="rounded-2xl  p-2">
-              <DeviceMobileIcon className="h-6 w-6 stroke-current text-gray-400" />
+            <button className="rounded-2xl p-3 text-gray-400 hover:bg-gray-100">
+              <DeviceMobileIcon className="h-6 w-6 stroke-current" />
             </button>
-            <button className="rounded-2xl  p-2">
-              <DeviceMobilerotatedIcon className="h-6 w-6 stroke-current text-gray-400" />
+            <button className="rounded-2xl p-3 text-gray-400 hover:bg-gray-100">
+              <DeviceMobilerotatedIcon className="h-6 w-6 stroke-current" />
             </button>
           </div>
 
@@ -71,7 +125,7 @@ function App() {
           </button>
         </header>
       </div>
-      <aside className="flex min-h-screen w-[300px] flex-col border border-gray-200 bg-white">
+      <aside className="flex min-h-screen w-[300px] flex-col border-l border-gray-200 bg-white">
         <div className="flex h-20 items-center gap-x-4 border-b border-gray-200 px-6">
           <button className="flex items-center justify-center gap-x-2 rounded-2xl bg-gray-100 px-5 py-3">
             <UsersIcon className="h-6 w-6 stroke-current text-gray-400" />
