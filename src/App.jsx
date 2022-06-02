aa7d5cb62bc6aac2fa99caa07ee29693f15ffc57
Misbah Ansori <misbah.ansori24@gmail.com>
2022-06-02 11:31:47 +0700

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -21,6 +21,7 @@ import AlignLeftIcon from "./icons/align-left.svg?component";
 import AlignRightIcon from "./icons/align-right.svg?component";
 import AlignTopIcon from "./icons/align-top.svg?component";
 import AlignMiddleIcon from "./icons/align-middle.svg?component";
+import BarsIcon from "./icons/bars.svg?component";
 import Tooltip from "./components/Tooltip";
 
 function App() {
@@ -99,7 +100,7 @@ function App() {
           </button>
         </div>
       </aside>
-      <div className="flex min-h-screen flex-1 flex-col">
+      <div className="flex h-screen flex-1 flex-col">
         <header className="flex h-18 items-center justify-center gap-x-4 border-b border-gray-200 bg-white px-8">
           <button className="flex items-center justify-center rounded-xl bg-gray-100 p-2">
             <ChevronLeftIcon className="h-6 w-6 stroke-current text-gray-400" />
@@ -150,12 +151,71 @@ function App() {
             <PackageIcon className="h-6 w-6 stroke-current text-gray-400" />
           </button>
         </header>
-        <main className="overflow-y-auto px-12">
-          <iframe
-            className="my-12 min-h-screen w-full"
-            src="https://www.dipainhouse.com/"
-            frameBorder="0"
-          ></iframe>
+        <main className="flex-1 overflow-y-scroll px-12">
+          <div className="my-12 bg-white">
+            <nav className="flex items-center justify-between px-12 py-6">
+              <img className="h-11 w-11" src="/img/logo.png" alt="" />
+              <div className="flex gap-x-6">
+                <button className="rounded-full bg-blue-600 py-3 px-8 text-white">
+                  Hire Us
+                </button>
+                <button className="p-2">
+                  <BarsIcon className="h-6 w-6 stroke-current" />
+                </button>
+              </div>
+            </nav>
+            <section className="py-16">
+              <div className="px-12">
+                <h1 className="max-w-3xl text-5xl font-bold leading-[1.4]">
+                  We craft digital products for business and user goals.
+                </h1>
+                <p className="mt-4 text-lg leading-loose text-gray-400">
+                  Help find solutions with UI / UX designs that are intuitive
+                  and in accordance with client business goals. We provide a
+                  high-quality service in UI/ UX Design & Development.
+                </p>
+              </div>
+              <div className="flex items-center gap-8 px-12 pt-8">
+                <button className="rounded-full bg-blue-600 px-8 py-5 font-semibold text-white">
+                  Let's work together
+                </button>
+                <button className="rounded-full border border-gray-200 px-8 py-5 font-semibold text-blue-600">
+                  Check our work
+                </button>
+              </div>
+            </section>
+            <section className="bg-gray-50 py-16">
+              <div className="px-12">
+                <span className="text-sm uppercase text-gray-400">
+                  Our Projects
+                </span>
+                <div className="flex items-center justify-between">
+                  <h2 className="max-w-3xl text-4xl font-bold leading-[1.4]">
+                    Our latest cool projects.
+                  </h2>
+                  <button className="rounded-full bg-white px-8 py-5 font-semibold text-blue-600">
+                    Check our work
+                  </button>
+                </div>
+              </div>
+              <div className="grid grid-cols-2 gap-8 px-12 pt-8">
+                <div className="flex aspect-square w-full items-center overflow-hidden bg-gray-100">
+                  <img
+                    className="h-full w-full object-cover"
+                    src="/img/billy.png"
+                    alt=""
+                  />
+                </div>
+                <div className="flex aspect-square w-full items-center overflow-hidden bg-gray-100">
+                  <img
+                    className="h-full w-full object-cover"
+                    src="/img/alpha-pay.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </section>
+          </div>
         </main>
       </div>
       <aside className="flex h-screen w-[300px] flex-col border-l border-gray-200 bg-white">
